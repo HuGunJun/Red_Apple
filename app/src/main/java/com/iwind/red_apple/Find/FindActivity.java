8407b1a29b33c6af1d4fec9562372e35c8ee333f
huguojun <www.guojunkuaile@qq.com>
2016-07-13 10:57:35 +0800

    修改发现页面 添加图片

--- app/src/main/java/com/iwind/red_apple/Find/FindActivity.java
+++ app/src/main/java/com/iwind/red_apple/Find/FindActivity.java
@@ -1,11 +1,16 @@
 package com.iwind.red_apple.Find;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.iwind.red_apple.R;
+import com.iwind.red_apple.Tax.NewActivity;
+import com.iwind.red_apple.Tax.PersonalTaxActivity;
+import com.iwind.red_apple.Tax.ShouldKnowActivity;
+import com.iwind.red_apple.Tax.TaxKnowActivity;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.x;
@@ -30,15 +35,20 @@ public class FindActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.rl_news:
+            case R.id.rl_news://最新资讯
+                startActivity(new Intent(this, NewActivity.class));
                 break;
-            case R.id.lv_guide_video:
+            case R.id.lv_guide_video://视频指导
+//                startActivity(new Intent(this, NewActivity.class));
                 break;
-            case R.id.lv_should_know:
+            case R.id.lv_do_tax_know://办税须知
+                startActivity(new Intent(this, ShouldKnowActivity.class));
                 break;
-            case R.id.lv_nomal_know:
+            case R.id.lv_nomal_know://税务常识
+                startActivity(new Intent(this, TaxKnowActivity.class));
                 break;
-            case R.id.lv_personal_industry:
+            case R.id.lv_personal_industry://个人业务
+                startActivity(new Intent(this, PersonalTaxActivity.class));
                 break;
         }
     }

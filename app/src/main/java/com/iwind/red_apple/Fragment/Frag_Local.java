bb91ba0f997614e0e687a08303c738b5b6a81017
huguojun <www.guojunkuaile@qq.com>
2016-07-18 12:58:06 +0800

    地税客户端接口添加

--- app/src/main/java/com/iwind/red_apple/Fragment/Frag_Local.java
+++ app/src/main/java/com/iwind/red_apple/Fragment/Frag_Local.java
@@ -10,14 +10,21 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Toast;
 
+import com.easemob.easeui.utils.ResponseUtils;
 import com.easemob.easeui.widget.xlistview.XListView;
 import com.iwind.red_apple.Adapter.NewsAdapter;
 import com.iwind.red_apple.Adapter.VideoGuideAdapter;
 import com.iwind.red_apple.Constant.ConstantString;
+import com.iwind.red_apple.Constant.ConstantUrl;
 import com.iwind.red_apple.R;
 import com.iwind.red_apple.Tax.NewsDetailActivity;
 import com.iwind.red_apple.Video.VideoDetailActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
@@ -36,7 +43,7 @@ public class Frag_Local extends BaseFragment {
     View view;
     @ViewInject(R.id.lv_local)
     XListView lv_local;
-
+    int page;
 
     private List<HashMap<String, String>> mList = new ArrayList<HashMap<String, String>>();
     private VideoGuideAdapter mHomePageAdapter;
@@ -64,13 +71,65 @@ public class Frag_Local extends BaseFragment {
     }
 
     public void InitData() {
-        for (int i = 0; i < 20; i++) {
-            HashMap<String, String> hashMap = new HashMap<String, String>();
-            mList.add(hashMap);
-        }
-        mHomePageAdapter = new VideoGuideAdapter(getActivity(), mList);
-        lv_local.setAdapter(mHomePageAdapter);
-        mHomePageAdapter.notifyDataSetChanged();
+        ShowLoadingDialog();
+        RequestParams params = new RequestParams(ConstantUrl.BASE_URL + ConstantUrl.GETCLIENT);
+        params.addBodyParameter(ConstantString.SEARCH_CONTENT, "");
+        params.addBodyParameter(ConstantString.ROWS, ConstantString.ROWCOUNT);
+        params.addBodyParameter(ConstantString.PAGE, "" + page);
+        x.http().post(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                lv_local.stopRefresh();
+                lv_local.stopLoadMore();
+                Log(result);
+                CloseLoadingDialog();
+                if (ResponseUtils.isSuccess(getActivity(), ConstantString.RESULT_STATE, result,
+                        ConstantString.STATE,
+                        ConstantString.RESULT_INFO)) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(result);
+                        JSONArray jsonArray = jsonObject.getJSONArray(ConstantString.ARRAY);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            HashMap<String, String> hashMap = new HashMap<String, String>();
+                            hashMap.put(ConstantString.CLIENT_ID, jsonArray.getJSONObject(i).getString(ConstantString
+                                    .CLIENT_ID));
+                            hashMap.put(ConstantString.CLIENT_TITLE, ResponseUtils.ParaseNull(jsonArray.getJSONObject
+                                    (i).getString
+                                    (ConstantString.CLIENT_TITLE)));
+                            hashMap.put(ConstantString.CLIENT_CONTENT, ResponseUtils.ParaseNull(jsonArray
+                                    .getJSONObject(i).getString
+                                            (ConstantString.CLIENT_CONTENT)));
+                            hashMap.put(ConstantString.MESSAGE_COUNT, ResponseUtils.ParaseNull(jsonArray
+                                    .getJSONObject(i).getString(ConstantString.MESSAGE_COUNT)).equals("") ? "0" :
+                                    ResponseUtils.ParaseNull(jsonArray
+                                            .getJSONObject(i).getString(ConstantString.MESSAGE_COUNT)));
+                            mList.add(hashMap);
+                        }
+                        mHomePageAdapter = new VideoGuideAdapter(getActivity(), mList);
+                        lv_local.setAdapter(mHomePageAdapter);
+                        mHomePageAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+                CloseLoadingDialog();
+            }
+        });
+
 
     }
 
@@ -79,6 +138,20 @@ public class Frag_Local extends BaseFragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 startActivity(new Intent(getActivity(), VideoDetailActivity.class));
+            }
+        });
+        lv_local.setXListViewListener(new XListView.IXListViewListener() {
+            @Override
+            public void onRefresh() {
+                mList.clear();
+                page = 0;
+                InitData();
+            }
+
+            @Override
+            public void onLoadMore() {
+                page++;
+                InitData();
             }
         });
     }
